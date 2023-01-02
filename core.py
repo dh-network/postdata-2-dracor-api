fb6603105c6fa13e0423bcf0c8f4b4dd8e7be0c0
Ingo Börner <ingoboerner@gmx.at>
2023-01-02 15:41:18 +0100

    minor fix

--- core.py
+++ core.py
@@ -28,19 +28,31 @@ class PostdataCorpus(Corpus):
 
         Args:
             database: connection to a triple store. Use instance of class DB.
+
+        TODO: maybe do not hardcode the queries (using the classes) but initialize with instances derived from them.
         """
         if database:
             self.database = database
 
     def get_poem_uris(self) -> list:
+        """Get a list of URIs of instances of the class pdc:PoeticWork.
+
+        Uses a Sparql Query of class "PoeticWorkUris" of the module "pd_stardog_queries".
+
+        Returns:
+            list: URIs of instances of class pdc:PoeticWork
+
+        """
         # check, if the poem uris have already been loaded, no need to load them again
-        query = PoeticWorkUris()
-        query.execute(self.database)
-        uris = query.results.simplify()
-        return uris
-
-
-
-
-
-
+        if self.poem_uris:
+            return self.poem_uris
+        else:
+            if self.database:
+                # A database connection has been established:
+                # Use the SPARQL Query of class "PoeticWorkUris" to retrieve the uris and simplify the results
+                # If other query should be used, this needs to be made dynamic (e.g. pass a query instance or store them
+                # in this class
+                self.poem_uris = PoeticWorkUris(database=self.database, execute=True).results.simplify()
+                return self.poem_uris
+            else:
+                raise Exception("Database Connection not available.")
